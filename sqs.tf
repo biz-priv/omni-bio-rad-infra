86f47cde2a11b6bd023ba6335831882f6aa41dba
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-05-13 18:17:53 +0530

    updated 30 to 900 sec for order events seconds

--- sqs.tf
+++ sqs.tf
@@ -3,6 +3,6 @@ resource "aws_sqs_queue" "omni_bio_rad_send_order_events_sqs" {
   delay_seconds             = 0
   max_message_size          = 262144
   message_retention_seconds = 345600
-  visibility_timeout_seconds = 30
+  visibility_timeout_seconds = 900
   receive_wait_time_seconds = 0
 }
