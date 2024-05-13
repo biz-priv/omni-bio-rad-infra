9bc40df38470e65f4c56e18b0ee4ff44115690bb
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-05-13 23:07:29 +0530

    added subscription for the send order events sqs

--- sqs.tf
+++ sqs.tf
@@ -6,3 +6,15 @@ resource "aws_sqs_queue" "omni_bio_rad_send_order_events_sqs" {
   visibility_timeout_seconds = 900
   receive_wait_time_seconds = 0
 }
+
+resource "aws_sns_topic_subscription" "omni_shipment_milestone_stream_sns_subscription" {
+  topic_arn = "arn:aws:sns:us-east-1:${var.aws_account_number}:omni-wt-rt-shipment-milestone-${var.env}"
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.omni_bio_rad_send_order_events_sqs.arn
+}
+
+resource "aws_sns_topic_subscription" "omni_apar_failure_stream_sns_subscription" {
+  topic_arn = "arn:aws:sns:us-east-1:${var.aws_account_number}:omni-wt-rt-apar-failure-${var.env}"
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.omni_bio_rad_send_order_events_sqs.arn
+}
