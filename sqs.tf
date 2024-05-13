168ccdf79ef845f0f3d267c9a164f6c8257f08fd
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-05-13 16:56:27 +0530

    added sqs queue for order events

---
+++ sqs.tf
@@ -0,0 +1,8 @@
+resource "aws_sqs_queue" "omni_bio_rad_send_order_events_sqs" {
+  name                      = "omni-bio-rad-send-order-events-sqs"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 345600
+  visibility_timeout_seconds = 30
+  receive_wait_time_seconds = 0
+}
